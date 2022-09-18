14dad025727731de4c068b353c680596e3988729
Liam James <liam.james@ymail.com>
2022-09-18 16:29:07 -0500

    transparency test and automatic config creation

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -2,20 +2,48 @@ use device_query::{DeviceQuery, DeviceState, Keycode};
 use simple_websockets::{Event, Responder};
 use config::Config;
 
+use std::io::Write;
 use std::sync::{Arc, Mutex};
 
 use std::collections::HashMap;
-use array_tool::vec::Intersect;
 use std::thread;
+use std::path::Path;
+use std::fs::File;
+
+use array_tool::vec::Intersect;
+
+static DEFAULT_CONFIG: &[u8] = 
+b"{
+    \"port\": 7685,
+    \"keys\": [ \"Z\", \"X\" ]
+}";
+
+fn create_default_config(name: &str) {
+    let mut file = File::create(name).unwrap();
+    file.write_all(DEFAULT_CONFIG).unwrap();
+}
+
+fn try_get_config(name: &str) -> Config {
+    if !Path::new(name).exists() {
+        create_default_config(name);
+    }
+
+    let result =
+        Config::builder()
+        .add_source(config::File::with_name(name))
+        .build();
+    
+    return match result {
+        Ok(file) => file,
+
+        other_error => panic!("Failed to open configuration file. {:?}", other_error)
+    };
+}
 
 fn main() {
-    let settings = 
-        Config::builder()
-        .add_source(config::File::with_name("settings"))
-        .build()
-        .unwrap();
-    
-    let client_list: Arc<Mutex<HashMap<u64, Responder>>> = Arc::new(Mutex::new(HashMap::new()));
+    // variables
+    let settings = try_get_config("settings.json"); 
+    let client_list = Arc::new(Mutex::new(HashMap::new()));
 
     let port = settings.get::<u16>("port").unwrap();
     let web_clone = Arc::clone(&client_list);
@@ -27,6 +55,8 @@ fn main() {
         let mut clients: HashMap<u64, Responder> = HashMap::new();
 
         loop {
+            // socket.poll_event is a blocking function, so we made a new thread for it.
+            // when something happens, it will exit the match statement, update `list` and then resume waiting
             match socket.poll_event() {
                 Event::Connect(client_id, responder) => {
                     println!("Client #{} connected.", client_id);
