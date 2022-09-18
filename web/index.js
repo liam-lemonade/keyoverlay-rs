78264664d9447358417b01a4409e543dac145d7c
Liam James <liam.james@ymail.com>
2022-09-17 23:48:51 -0500

    basic web keypress counter with odometer

--- web/index.js
+++ web/index.js
@@ -1,7 +1,13 @@
-const daemonPort = 7685;
-let socket = new ReconnectingWebSocket('ws://127.0.0.1:' + daemonPort + '/ws');
+class Settings {
+    static port = 7685;
+    static odometerAnimation = true;
+    static odometerAnimationSpeedMs = 100;
+}
+
+let socket = new ReconnectingWebSocket('ws://127.0.0.1:' + Settings.port + '/ws');
 
 socket.onopen = () => {
+    odometer.innerHTML = 0;
     console.log("Successfully Connected");
 };
 
@@ -14,5 +20,66 @@ socket.onerror = error => {
 };
 
 socket.onmessage = event => {
-    console.log(event.data);
+    handleKeyPress(event.data);
+    //console.log(event.data);
 }
+
+class Key {
+    text = "";
+    counter = 0;
+}
+
+let keysList = [];
+
+function handleKeyPress(data) {
+    if (data !== "[]") {
+        let pressedKeys = [];
+        
+        // Parse JSON that looks like ["X", "Z"]
+        let parsed = JSON.parse(data);
+
+        // For each key currently pressed
+        parsed.forEach(jsonKey => {
+            let found = null;
+            
+            keysList.every(storedKey => {
+                // if this key (keyString) has been pressed before
+                // don't use .includes because of `counter` variable
+                if (storedKey.text === jsonKey) {
+                    found = storedKey;
+
+                    return false; // .every breaks on return false
+                }
+
+                return true; // .every will also break if `true` is not returned at some point
+            });
+
+            // key is being pressed for the first time
+            if (found === null) {
+                let added = new Key();
+                added.text = jsonKey;
+                
+                keysList.push(added);
+                pressedKeys.push(added);
+            }
+            else {
+                // key exists in list
+                pressedKeys.push(found);
+            }
+        });
+
+        // increase pressed key counter by 1
+        pressedKeys.forEach(key => {
+            key.counter++
+        });
+
+        // update animation
+        if (Settings.odometerAnimation) {
+            // TODO: implement something that adds new odometers for every key in `keysList`
+            odometer.innerHTML = pressedKeys[0].counter;
+        }
+    }
+}
+
+// Main
+document.querySelector(':root').style.setProperty("--duration", Settings.odometerAnimationSpeedMs + "ms");
