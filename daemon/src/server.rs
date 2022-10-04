1fcd3d4a9b2638539f0464a05dd59a230b0bc299
Liam James <liam.james@ymail.com>
2022-10-04 14:14:54 -0500

    try implement websocket

--- daemon/src/server.rs
+++ daemon/src/server.rs
@@ -2,10 +2,16 @@ extern crate actix_files;
 extern crate actix_web;
 extern crate actix_web_actors;
 
+pub mod websocket;
+
 use actix_files as fs;
-use actix_web::{middleware::TrailingSlash, App, HttpServer};
+use actix_web::{
+    middleware::TrailingSlash, web, App, Error, HttpRequest, HttpResponse, HttpServer,
+};
 use actix_web_actors::ws;
 use actix_web_lab::middleware::NormalizePath;
+
+use websocket::WebSocket;
 
 use std::vec::Vec;
 
@@ -14,8 +20,19 @@ use crate::{error, settings::Settings};
 const WEBFILE_PATH: &str = r"D:\code\projects\keyoverlay\web";
 // const WEBFILE_PATH: &str = r"web"
 
-pub async fn update_clients(_pressed: Vec<String>) {
+pub async fn update_clients(pressed: Vec<String>) {
     // iterate through all websocket clients and send pressed to them
+    let _ = pressed.clone();
+}
+
+pub async fn websocket_connect(
+    request: HttpRequest,
+    stream: web::Payload,
+) -> Result<HttpResponse, Error> {
+    let response = ws::start(WebSocket {}, &request, stream);
+    println!("{:?}", response);
+
+    response
 }
 
 #[actix_web::main]
@@ -25,11 +42,14 @@ pub async fn spawn_server(settings: Settings) -> std::io::Result<()> {
     let wrapper = NormalizePath::new(TrailingSlash::Always).use_redirects();
 
     let server = HttpServer::new(move || {
-        App::new().wrap(wrapper).service(
-            fs::Files::new("/", WEBFILE_PATH)
-                .show_files_listing()
-                .index_file("index.html"),
-        )
+        App::new()
+            .wrap(wrapper)
+            .route("/ws", web::get().to(websocket_connect))
+            .service(
+                fs::Files::new("/", WEBFILE_PATH)
+                    .show_files_listing()
+                    .index_file("index.html"),
+            )
     })
     .bind(address)?
     .run();
